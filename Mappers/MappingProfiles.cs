0957653d7bc7d7571b86dc46c5ce069e58e58c2c
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-06-07 22:02:58 +0500

    Mappers

---
+++ Mappers/MappingProfiles.cs
@@ -0,0 +1,32 @@
+﻿
+
+using AutoMapper;
+using Library_Management_System.DTO;
+using Library_Management_System.Models;
+
+namespace Library_Management_System.Mappers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles() 
+        {
+            CreateMap<Users , UserDTO>();
+            CreateMap<UserDTO , Users>();
+
+            CreateMap<Books , BooksDTO>();
+            CreateMap<BooksDTO , Books>();
+
+            CreateMap<Loans, LoansDTO>();
+            CreateMap<Loans, LoansDTOWithoutReturn>();
+            CreateMap<LoansDTO, Loans>();
+            CreateMap<UpdateLoansDTO, Loans>();
+            CreateMap<LoansDTOWithoutReturn, Loans>();
+
+            CreateMap<Authors, AuthorDTO>();
+            CreateMap<AuthorDTO , Authors>();
+
+            CreateMap<BookAuthor , BookAuthorDTO>();
+            CreateMap<BookAuthorDTO , BookAuthor>();
+        }
+    }
+}
