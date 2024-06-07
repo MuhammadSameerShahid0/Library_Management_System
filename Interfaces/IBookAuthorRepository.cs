a172eb0bacfeed01336b104a7c18ccd73d1ca75f
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-06-07 20:52:53 +0500

    Interfaces

---
+++ Interfaces/IBookAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Library_Management_System.Models;
+
+namespace Library_Management_System.Interfaces
+{
+    public interface IBookAuthorRepository
+    {
+        void AddBookAuthor(BookAuthor bookAuthor);
+        IEnumerable<BookAuthor> IdBookAuthors(int bookId);
+        IEnumerable<BookAuthor> BooksByAuthor(int authorId);
+    }
+}
---
+++ Interfaces/ILoansRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library_Management_System.Models;
+
+namespace Library_Management_System.Interfaces
+{
+    public interface ILoansRepository
+    {
+        Task<Loans> AddLoan(Loans loan);
+        Loans GetLoan(int loanId);
+        IEnumerable<Loans> GetAllLoans();
+        Loans GetLoansByUser(int userId);
+        Task<Loans> UpdateLoan(Loans loan);
+    }
+}
