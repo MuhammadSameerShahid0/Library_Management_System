66e6720016a5f127d981f79c3e5cd124410dbaf8
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-06-07 20:33:59 +0500

    DTO

---
+++ DTO/BookAuthorDTO.cs
@@ -0,0 +1,10 @@
+﻿using Library_Management_System.Models;
+
+namespace Library_Management_System.DTO
+{
+    public class BookAuthorDTO
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ DTO/BooksDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Library_Management_System.DTO
+{
+    public class BooksDTO
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public string Genre { get; set; }
+    }
+}
