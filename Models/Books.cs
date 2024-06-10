4b854183ffc69159fd4748cb61402f15578d7c92
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-06-10 22:46:04 +0500

    Updated_Models

--- Models/Books.cs
+++ Models/Books.cs
@@ -10,5 +10,7 @@ namespace Library_Management_System.Models
         public string Author    { get; set; }
         public string ISBN      { get; set; }
         public string Genre     { get; set; }
+
+        public ICollection<BookAuthor> BookAuthors { get; set; }
     }
 }
