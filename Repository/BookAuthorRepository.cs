f58a72f5d00ba45c29e2d913947961db5e316d1e
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-06-10 23:43:18 +0500

    Updated_Repository

--- Repository/BookAuthorRepository.cs
+++ Repository/BookAuthorRepository.cs
@@ -1,4 +1,5 @@
-﻿using Library_Management_System.Interfaces;
+﻿using Library_Management_System.Data;
+using Library_Management_System.Interfaces;
 using Library_Management_System.Models;
 using System.Net;
 
@@ -6,21 +7,27 @@ namespace Library_Management_System.Repository
 {
     public class BookAuthorRepository : IBookAuthorRepository
     {
-        private readonly List<BookAuthor> _bookAuthors = new List<BookAuthor>();
-
-        public void AddBookAuthor(BookAuthor bookAuthor)
+        //private readonly List<BookAuthor> _bookAuthors = new List<BookAuthor>();
+        private readonly DataContext _context;
+        public BookAuthorRepository(DataContext context)
         {
-            _bookAuthors.Add(bookAuthor);
+            _context = context;
+        }
+
+        public async void AddBookAuthor(BookAuthor bookAuthor)
+        {
+            _context.BookAuthors.Add(bookAuthor);
+            await _context.SaveChangesAsync();
         }
 
         public IEnumerable<BookAuthor> BooksByAuthor(int authorId)
         {
-           return _bookAuthors.Where(ba => ba.AuthorId == authorId);
+           return _context.BookAuthors.Where(ba => ba.AuthorId == authorId);
         }
 
         public IEnumerable<BookAuthor> IdBookAuthors(int bookId)
         {
-            return _bookAuthors.Where(ba => ba.BookId == bookId);
+            return _context.BookAuthors.Where(ba => ba.BookId == bookId);
         }
     }
 }
--- Repository/LoansRepository.cs
+++ Repository/LoansRepository.cs
@@ -1,4 +1,5 @@
-﻿using Library_Management_System.Interfaces;
+﻿using Library_Management_System.Data;
+using Library_Management_System.Interfaces;
 using Library_Management_System.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using System.Collections.Generic;
@@ -8,34 +9,41 @@ namespace Library_Management_System.Repositories
 {
     public class LoanRepository : ILoansRepository
     {
-        private readonly List<Loans> _loans = new List<Loans>();
+        //private readonly List<Loans> _loans = new List<Loans>();
+        private readonly DataContext _context;
+        public LoanRepository(DataContext context)
+        {
+            _context = context;
+        }
         public async Task<Loans> AddLoan(Loans loan)
         {
-            _loans.Add(loan);
+            _context.Loans.Add(loan);
+            await _context.SaveChangesAsync();  
             return loan;
         }
         public IEnumerable<Loans> GetAllLoans()
         {
-            return _loans.OrderBy(L => L.LoanID).ToList(); 
+            return _context.Loans.OrderBy(L => L.LoanID).ToList(); 
         }
         public Loans GetLoan(int loanId)
         {
-           return _loans.FirstOrDefault(l => l.LoanID == loanId);
+           return _context.Loans.FirstOrDefault(l => l.LoanID == loanId);
         }
         public Loans GetLoansByUser(int userId)
         {
-            return _loans.Where(u => u.UserID == userId).FirstOrDefault();
+            return _context.Loans.Where(u => u.UserID == userId).FirstOrDefault();
         }
 
         public async Task<Loans> UpdateLoan(Loans loan)
         {
-            var existingLoan = _loans.FirstOrDefault(ul => ul.LoanID == loan.LoanID);
+            var existingLoan = _context.Loans.FirstOrDefault(ul => ul.LoanID == loan.LoanID);
             if (existingLoan != null)
             {
                 existingLoan.LoanID = loan.LoanID;
                 existingLoan.LoanDate = loan.LoanDate;
                 existingLoan.ReturnDate = loan.ReturnDate;
             }
+            await _context.SaveChangesAsync();
             return existingLoan;
         }
     }
